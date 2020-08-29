6da1a88e8bac44f32e12f1441d1b0ef727d24586
Denis Kozlov <bbuterbrott@gmail.com>
2020-08-29 15:07:41 +0300

    Исправление по код ревью

--- hw01_hello_now/main.go
+++ hw01_hello_now/main.go
@@ -16,9 +16,8 @@ func main() {
 	t := time.Now()
 	fmt.Printf("current time: %v\n", t.Round(0))
 	ntpTime, err := ntp.Time(ntpServer)
-	if err == nil {
-		fmt.Printf("exact time: %v\n", ntpTime.Round(0))
-	} else {
+	if err != nil {
 		log.Fatalf("Got error while getting time from ntp server %v: %v", ntpServer, err)
 	}
+	fmt.Printf("exact time: %v\n", ntpTime.Round(0))
 }
