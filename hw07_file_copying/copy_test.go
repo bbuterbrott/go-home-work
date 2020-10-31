9ed62106b44d105d706051fd8293da856625f4f3
Denis Kozlov <bbuterbrott@gmail.com>
2020-10-31 13:50:15 +0300

    Добавление тестов

--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -17,9 +18,25 @@ func TestCopy(t *testing.T) {
 		testCopy(t, 100, 1000)
 		testCopy(t, 6000, 1000)
 	})
+
+	t.Run("from file doesn't exist", func(t *testing.T) {
+		err := Copy("testdata/input.txt1111111", "/tmp/out.txt", 0, 100)
+		require.EqualError(t, err, ErrUnsupportedFile.Error())
+	})
+
+	t.Run("empty file", func(t *testing.T) {
+		err := Copy("testdata/empty.txt", "/tmp/out.txt", 0, 100)
+		require.EqualError(t, err, ErrUnsupportedFile.Error())
+	})
+
+	t.Run("offset greater then file size", func(t *testing.T) {
+		err := Copy("testdata/input.txt", "/tmp/out.txt", 50000000000, 50000000001)
+		require.EqualError(t, err, ErrOffsetExceedsFileSize.Error())
+	})
 }
 
 func testCopy(t *testing.T, offset, limit int64) {
+	os.Remove("/tmp/out.txt")
 	err := Copy("testdata/input.txt", "/tmp/out.txt", offset, limit)
 	require.NoError(t, err)
 	result, err := ioutil.ReadFile("/tmp/out.txt")
