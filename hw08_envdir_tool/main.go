5b611e46c272227d877bd04e0c1085a9d1df6f33
Denis Kozlov <bbuterbrott@gmail.com>
2020-11-07 16:27:19 +0300

    Исправление main

--- hw08_envdir_tool/main.go
+++ hw08_envdir_tool/main.go
@@ -6,12 +6,12 @@ import (
 )
 
 func main() {
-	envDir := os.Args[0]
+	envDir := os.Args[1]
 	env, err := ReadDir(envDir)
 	if err != nil {
 		fmt.Printf("Couldn't read environment variables from dir '%v'\n", envDir)
 		os.Exit(111)
 	}
-	returnCode := RunCmd(os.Args[1:], env)
+	returnCode := RunCmd(os.Args[2:], env)
 	os.Exit(returnCode)
 }
